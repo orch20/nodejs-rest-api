abcca855a48cb8532f18e4dec6e52b89d6105f8b
orch20 <94862153+orch20@users.noreply.github.com>
2022-11-01 19:49:38 +0200

    added updateAvatar controller and middleware upload

---
+++ controlers/auth/updateAvatar.js
@@ -0,0 +1,23 @@
+const { User } = require("../../models/user");
+const fs = require("fs/promises");
+const path = require("path");
+
+const avatarDir = path.join(__dirname, "../../", "public", "avatars");
+
+const updateAvatar = async (req, res, next) => {
+  try {
+    const { _id } = req.user;
+    const { path: tempUpload, originalname } = req.file;
+    const extension = originalname.split(".").pop();
+    const filename = `${_id}.${extension}`;
+    const resultUpload = path.join(avatarDir, filename);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("avatars", filename);
+    await User.findByIdAndUpdate(_id, { avatarURL });
+    res.json({ avatarURL });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = updateAvatar;
--- models/user.js
+++ models/user.js
@@ -33,6 +33,9 @@ const userSchema = new Schema(
       type: String,
       default: "",
     },
+    avatarURL: {
+      type: String,
+    },
   },
   { versionKey: false, timestamps: true }
 );
