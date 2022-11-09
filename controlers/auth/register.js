83c5f6aa3c015dd785c279a6137938642e65a7d6
orch20 <94862153+orch20@users.noreply.github.com>
2022-11-10 00:14:18 +0200

    added user verification by email

--- controlers/auth/register.js
+++ controlers/auth/register.js
@@ -1,7 +1,8 @@
 const { User } = require("../../models/user");
-const { RequestError } = require("../../helpers");
+const { RequestError, sendEmail, createVerifyEmail } = require("../../helpers");
 const bcrypt = require("bcrypt");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
 
 const register = async (req, res, next) => {
   try {
@@ -13,13 +14,19 @@ const register = async (req, res, next) => {
 
     const hashPassword = await bcrypt.hash(password, 10);
     const avatarURL = gravatar.url(email);
-
+    const verificationToken = nanoid();
     const result = await User.create({
       name,
       email,
       password: hashPassword,
       avatarURL,
+      verificationToken,
     });
+
+    const mail = createVerifyEmail(email, verificationToken);
+
+    await sendEmail(mail);
+
     res.status(201).json({ name: result.name, email: result.email });
   } catch (error) {
     next(error);
--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,11 @@
 const RequestError = require("./RequestError");
 const handleSaveErrors = require("./handleSaveErrors");
+const sendEmail = require("./sendEmail");
+const createVerifyEmail = require("./createVerifyEmail");
 
 module.exports = {
   RequestError,
   handleSaveErrors,
+  sendEmail,
+  createVerifyEmail,
 };
